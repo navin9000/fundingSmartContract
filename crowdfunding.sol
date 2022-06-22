768391fcc2d79cc8581216a10c51dde9c38e150e
naveen pulamarasetti <naveen95736@gmail.com>
2022-06-22 23:39:58 +0530

    added events and modifiers

--- crowdfunding.sol
+++ crowdfunding.sol
@@ -14,6 +14,34 @@ contract Funding{
     uint endTime;
     uint startTime;
 
+    //events
+    event invested(address from,uint value);
+    event toManager(address from,address to,uint value);
+    event toInvsetors(address from,address to,uint value);
+    
+    // function modifier
+     modifier checkInvestor{
+        require(manager!=msg.sender); 
+        require(msg.value > 0 wei,"not enough funds"); 
+        require(block.timestamp < endTime,"funding pool expired");   
+        require(amntRequired() > 0,"funding goal reached"); 
+        _;
+     }
+
+     //function modifier fund to manger
+     modifier fundToManager{
+        require(block.timestamp > endTime); 
+        require(totalAmt >= goalAmt);
+        require(investors[msg.sender] <= 0,"claimed aleardy");
+        _;
+     }
+
+     //function modifier funds back to investors
+     modifier fundToInvestors{
+        require(block.timestamp > endTime);
+        require(totalAmt < goalAmt);
+        _;
+     }
 
     //constructor for intializing manager 
     constructor(uint _amt,uint _time){
@@ -25,14 +53,11 @@ contract Funding{
 
     //getting fund amount to contract and checking for the conditions to met
     //the first and last person who is investing can invest more than required
-    function toGetFunds() public payable {
-        require(manager!=msg.sender); 
-        require(msg.value > 0 wei,"not enough funds"); 
-        require(block.timestamp < endTime,"funding pool expired");    //20  < 20 endTime
-        require(amntRequired() > 0,"funding goal reached"); //accepting investors. // 50 wei -->30 wei  = 50-30= 
+    function toGetFunds() public payable checkInvestor{
         investors[msg.sender]=msg.value;
         totalAmt+=msg.value;
         noOfInvestors+=1; //rewards
+        emit invested(msg.sender,msg.value);
     }
 
     //there are two outcomes for our project
@@ -40,18 +65,17 @@ contract Funding{
    
     //1.If successful funding 
     // so funds will transfer to the fund raiser
-    function transferFundsToManager() public{
-        require(block.timestamp > endTime); 
-        require(totalAmt >= goalAmt);
+    function FundsToManager() public payable fundToManager{
         manager.transfer(address(this).balance);
+        emit toManager(address(this),manager,address(this).balance);
     }
 
     //2.If the funding is failed
     //here refunding the funds to the investors if funding goal is not reached
-    function reFundingToInvestors() public view { //10eth        1,1,1,
-        require(block.timestamp > endTime);
-        require(totalAmt < goalAmt);
-
+    function reFundingToInvestors() public payable fundToInvestors{
+        payable(msg.sender).transfer(investors[msg.sender]);
+        emit toInvsetors(address(this),msg.sender,investors[msg.sender]);
+        investors[msg.sender]=0;
     }
 
     //to check the amount required
